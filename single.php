4cee2f39b9c9540191c28acbd39901ac11b9e2ad
heera9331 <heera9331@gmail.com>
2024-05-13 00:37:39 +0530

    single.php

--- single.php
+++ single.php
@@ -7,16 +7,15 @@
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <?php // get_template_part('template-parts/content') ?>
-        <?php get_template_part('template-parts/content', 'page') ?>
+        <?php get_template_part('template-parts/content') ?>
+        <?php get_template_part('template-parts/content', 'none') ?>
 
         <?php get_sidebar('main-sidebar'); ?>
     </main>
 </div>
-<h3>Template: singular.php</h3>
-
+<!-- behave like a template/layout for single blog page -->
+<h3>Template: single.php</h3>
 <?php get_footer(); ?>
-
 </body>
 
 </html>
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,6 +1,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h2>index.php</h2>
+        <h2><?php the_title(); ?></h2>
+        <p> <strong> <?php esc_html__("Author: "); ?> - </strong> <?php the_author(); ?></p>
+        <p> <strong> <?php esc_html__("Date: "); ?> - </strong> <?php the_date(); ?></p>
     </header>
     <div class="entry-content">
         <p>article into template-part/content-none.php</p>
